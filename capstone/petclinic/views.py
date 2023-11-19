c7f435620dca4738f2ed314bce3dc586cc3fe0b8
basnaly <basnaly@gmail.com>
2023-11-19 18:58:22 +0200

    Add login and register html files, styling

--- capstone/petclinic/views.py
+++ capstone/petclinic/views.py
@@ -13,13 +13,13 @@ def index(request):
     return render(request, "petclinic/index.html")
 
 
-def login(request):
+def login_view(request):
     if request.method == "POST":
         
         # Attempt to sign user in
         identical_number = request.POST["identical_number"]
         password = request.POST["password"]
-        user = authenticate(request, identical_number=identical_number, password=password)
+        user = authenticate(request, username=identical_number, password=password)
         
         # Check if authentication successful
         if user is not None:
@@ -27,7 +27,7 @@ def login(request):
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "petclinic/login.html", {
-                "message": "Invalid identical_number or password"
+                "message": "Invalid identical number or password!"
             })
     else:    
         return render(request, "petclinic/login.html")
@@ -51,19 +51,26 @@ def register(request):
             
         # Attempt to create new user
         try:
-            new_user = User.objects.create_user(identical_number, first_name, last_name, phone_number, email, password)
-            new_user.save()
+            user = User.objects.create_user(
+                username = identical_number, 
+                first_name = first_name, 
+                last_name = last_name, 
+                phone_number = phone_number, 
+                email = email, 
+                password = password
+            )
+            user.save()
         except IntegrityError:
             return render(request, "petclinic/register.html", {
-                "message": "User already taken"
+                "message": "User already taken!"
             })
-        login(request, new_user)
+        login(request, user)
         return HttpResponseRedirect(reverse("index"))
     
     else:
         return render(request, "petclinic/register.html")
 
 
-def logout(request):
+def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
